592cd3e1b1c4790ffd41cbe689a5529b571cafbe
Szczepan Faber <sfaber@linkedin.com>
2017-03-21 13:07:12 -0700

    Starting point tune-up

--- src/test/java/SmartDictionaryTest.java
+++ src/test/java/SmartDictionaryTest.java
@@ -17,8 +17,7 @@ public class SmartDictionaryTest {
     DictionaryHistory history = mock(DictionaryHistory.class);
     SmartDictionary dictionary = new SmartDictionary(wiki, history);
 
-    @Test
-    public void should_look_up_words() throws Exception {
+    @Test public void should_look_up_words() throws Exception {
         //given
         given(wiki.findDescription("mockito")).willReturn("Mocking framework");
 
@@ -29,8 +28,7 @@ public class SmartDictionaryTest {
         assertEquals("Mocking framework", description);
     }
 
-    @Test
-    public void should_keep_history() throws Exception {
+    @Test public void should_keep_history() throws Exception {
         //when
         dictionary.lookUp("mockito");
 
@@ -38,8 +36,7 @@ public class SmartDictionaryTest {
         verify(history).lookUpAttempt("mockito");
     }
 
-    @Test
-    public void should_ignore_history_failures() throws Exception {
+    @Test public void should_ignore_history_failures() throws Exception {
         //given
         willThrow(new HistoryFailureException()).given(history).lookUpAttempt("mockito");
 
